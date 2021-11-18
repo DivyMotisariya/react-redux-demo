16963d501c0d357601e9a1a7589623132704254b
DivyMotisariya <motisariyadivy2@gmail.com>
2021-11-18 12:28:47 +0530

    basic middleware to log everything

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const redux = require("redux");
-const { createStore, combineReducers } = redux;
+const { createStore, combineReducers, applyMiddleware } = redux;
+const { createLogger } = require("redux-logger");
+const logger = createLogger();
 
 const BUY_CAKE = "BUY_CAKE";
 const RETURN_CAKE = "RETURN_CAKE";
@@ -91,13 +93,11 @@ const rootReducer = combineReducers({
   iceCream: iceCreamReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 console.log("initial state", store.getState());
 
-const unsubscribe = store.subscribe(() =>
-  console.log("updated state", store.getState())
-);
+const unsubscribe = store.subscribe(() => {});
 
 store.dispatch(buyCake());
 store.dispatch(buyCake());
